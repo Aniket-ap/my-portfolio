b386f5f3e1d714733fcc17965755973017eab5eb
Aniket Pradhan <aniketpradhan814@gmail.com>
2022-04-19 16:53:45 +0530

    create an array for store all the project details

---
+++ src/data.js
@@ -0,0 +1,39 @@
+import portfolioImg from "./assets/image/portfolio-ss.jpg"
+import blogHashImg from "./assets/image/bloghash-ss.jpg"
+import imdbImg from "./assets/image/imdb-ss.jpg"
+import ticTacToeImg from "./assets/image/tic-tac-ss.jpg"
+
+export const PROJECT_DETAILS = [
+  {
+    id: 1,
+    title: "My Portfolio",
+    description: "My Portfolio website using React Js.",
+    pic: portfolioImg,
+    viewLink: "https://aniket08-portfolio.netlify.app/",
+    sourceCode: "https://github.com/Aniket-ap/my-portfolio",
+  },
+  {
+    id: 2,
+    title: "BlogHash",
+    description: "A blog app where user user can post blog.it is a MERN stack project",
+    pic: blogHashImg,
+    viewLink: "https://helpful-medovik-b34862.netlify.app/",
+    sourceCode: "https://github.com/Aniket-ap/my-portfolio",
+  },
+  {
+    id: 3,
+    title: "IMDB Clone",
+    description: "A clone of popular movie review website IMDB using React Js.",
+    pic: imdbImg,
+    viewLink: "https://pedantic-ptolemy-a223c9.netlify.app/",
+    sourceCode: "https://github.com/Aniket-ap/imdb-clone",
+  },
+  {
+    id: 4,
+    title: "TIC TAC TOE",
+    description: "A simple tic tac toe game made using pure HTML, CSS3, JS.",
+    pic: ticTacToeImg,
+    viewLink: "https://aniket-ap.github.io/TicTacToe.github.io/",
+    sourceCode: "https://github.com/Aniket-ap/TicTacToe.github.io",
+  }
+];
