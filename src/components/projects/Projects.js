52a3c00dd3c9711119375dead98bdef4ed852c60
Aniket Pradhan <aniketpradhan814@gmail.com>
2022-04-19 16:52:57 +0530

    display all projects in a card feature implemented

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import "./projects.css";
+import { PROJECT_DETAILS } from "../../data";
+import { Link } from "react-router-dom";
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <section className="all-projects">
+      <div className="projects">
+        {PROJECT_DETAILS.map((data) => (
+          <>
+            <div className="cards_item">
+              <div className="card">
+                <div className="card_image">
+                  <img src={data.pic} alt="project pic" />
+                </div>
+                <div className="card_content">
+                  <h2 className="card_title">{data.title}</h2>
+                  <p className="card_text">{data.description}</p>
+                  <div className="btn">
+                    <button>
+                      <Link
+                        to="#"
+                        onClick={() => (window.location = `${data.viewLink}`)}
+                      >
+                        View
+                      </Link>
+                    </button>
+                    <button>
+                      <Link
+                        to="#"
+                        onClick={() => (window.location = `${data.sourceCode}`)}
+                      >
+                        sourceCode
+                      </Link>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default Projects
+export default Projects;
