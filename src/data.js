a815ea85ef57e88bc97c10da07a5d23ed4d7dfa3
Aniket Pradhan <aniketpradhan814@gmail.com>
2022-07-30 00:47:32 +0530

    added html css project

--- src/data.js
+++ src/data.js
@@ -1,57 +1,208 @@
-import portfolioImg from "./assets/image/portfolio-ss.jpg"
-import blogHashImg from "./assets/image/bloghash-ss.jpg"
-import imdbImg from "./assets/image/imdb-ss.jpg"
-import ticTacToeImg from "./assets/image/tic-tac-ss.jpg"
-import githubSearchImg from "./assets/image/github-search.jpg"
-import spaceX from "./assets/image/spaceX.jpg"
+// import portfolioImg from "./assets/image/portfolio-ss.jpg"
+// import blogHashImg from "./assets/image/bloghash-ss.jpg"
+// import imdbImg from "./assets/image/imdb-ss.jpg"
+// import ticTacToeImg from "./assets/image/tic-tac-ss.jpg"
+// import githubSearchImg from "./assets/image/github-search.jpg"
+// import spaceX from "./assets/image/spaceX.jpg"
+import htmlP1 from "./assets/image/html-p1.jpg"
+import htmlP2 from "./assets/image/html-p2.jpg"
+import htmlP3 from "./assets/image/html-p3.jpg"
+import htmlP4 from "./assets/image/html-p4.jpg"
+import htmlP5 from "./assets/image/html-p5.jpg"
+import htmlP6 from "./assets/image/html-p6.jpg"
+import htmlP7 from "./assets/image/html-p7.jpg"
+import htmlP8 from "./assets/image/html-p8.jpg"
+import htmlP9 from "./assets/image/html-p9.jpg"
+import htmlP10 from "./assets/image/html-p10.jpg"
+import htmlP11 from "./assets/image/html-p11.jpg"
+import htmlP12 from "./assets/image/html-p12.jpg"
+import htmlP13 from "./assets/image/html-p13.jpg"
+import htmlP14 from "./assets/image/html-p14.jpg"
+import htmlP15 from "./assets/image/html-p15.jpg"
 
 export const PROJECT_DETAILS = [
   {
     id: 1,
-    title: "My Portfolio",
-    description: "My Portfolio website using React Js.",
-    pic: portfolioImg,
-    viewLink: "https://aniket08-portfolio.netlify.app/",
-    sourceCode: "https://github.com/Aniket-ap/my-portfolio",
+    title: "Project 1",
+    description: "Project 1",
+    pic: htmlP1,
+    tag: "html",
+    viewLink: "https://mellow-sunburst-2bb3cc.netlify.app/",
+    sourceCode: "https://github.com/Aniket-ap/HTML_CSS__project-1",
   },
   {
     id: 2,
-    title: "BlogHash",
-    description: "A blog app where user user can post blog.it is a MERN stack project",
-    pic: blogHashImg,
-    viewLink: "https://helpful-medovik-b34862.netlify.app/",
-    sourceCode: "https://github.com/Aniket-ap/my-portfolio",
+    title: "Project 2",
+    description: "Project 2",
+    pic: htmlP2,
+    tag: "html",
+    viewLink: "https://glittering-frangollo-2b3b56.netlify.app/",
+    sourceCode: "https://github.com/Aniket-ap/HTML_CSS__project-2",
   },
   {
     id: 3,
-    title: "Github Search",
-    description: "A github profile search app",
-    pic: githubSearchImg,
-    viewLink: "https://github-search-profile-app.netlify.app/",
-    sourceCode: "https://gitlab.com/Aniket-ap/github-search-app",
+    title: "Project 3",
+    description: "Project 3",
+    pic: htmlP3,
+    tag: "html",
+    viewLink: "https://elaborate-praline-9d92d2.netlify.app/",
+    sourceCode: "https://github.com/Aniket-ap/HTML_CSS__project-3",
   },
   {
     id: 4,
-    title: "IMDB Clone",
-    description: "A clone of popular movie review website IMDB using React Js.",
-    pic: imdbImg,
-    viewLink: "https://pedantic-ptolemy-a223c9.netlify.app/",
-    sourceCode: "https://github.com/Aniket-ap/imdb-clone",
+    title: "Project 4",
+    description: "Project 4",
+    pic: htmlP4,
+    tag: "html",
+    viewLink: "https://stalwart-biscotti-00d1a4.netlify.app/",
+    sourceCode: "https://github.com/Aniket-ap/HTML_CSS__project-4",
   },
   {
     id: 5,
-    title: "SpaceX launch data",
-    description: "An application having all the details of all spacex rocket launch data.",
-    pic: spaceX,
-    viewLink: "https://launch-spacex-data.netlify.app/",
-    sourceCode: "https://github.com/Aniket-ap/techdome-assignment",
+    title: "Project 5",
+    description: "Project 5",
+    pic: htmlP5,
+    tag: "html",
+    viewLink: "https://grand-sunshine-a003b3.netlify.app/",
+    sourceCode: "https://github.com/Aniket-ap/HTML_CSS__project-5",
   },
   {
     id: 6,
-    title: "TIC TAC TOE",
-    description: "A simple tic tac toe game made using pure HTML, CSS3, JS.",
-    pic: ticTacToeImg,
-    viewLink: "https://aniket-ap.github.io/TicTacToe.github.io/",
-    sourceCode: "https://github.com/Aniket-ap/TicTacToe.github.io",
-  }
+    title: "Project 6",
+    description: "Project 6",
+    pic: htmlP6,
+    tag: "html",
+    viewLink: "https://boisterous-griffin-9754af.netlify.app/",
+    sourceCode: "https://github.com/Aniket-ap/HTML_CSS__project-6",
+  },
+  {
+    id: 7,
+    title: "Project 7",
+    description: "Project 7",
+    pic: htmlP7,
+    tag: "html",
+    viewLink: "https://amazing-entremet-211b73.netlify.app/",
+    sourceCode: "https://github.com/Aniket-ap/HTML_CSS__project-7",
+  },
+  {
+    id: 8,
+    title: "Project 8",
+    description: "Project 8",
+    pic: htmlP8,
+    tag: "html",
+    viewLink: "https://incredible-parfait-b3218a.netlify.app/",
+    sourceCode: "https://github.com/Aniket-ap/HTML_CSS__project-8",
+  },
+  {
+    id: 9,
+    title: "Project 9",
+    description: "Project 9",
+    pic: htmlP9,
+    tag: "html",
+    viewLink: "https://chimerical-creponne-b82095.netlify.app/",
+    sourceCode: "https://github.com/Aniket-ap/HTML_CSS__project-9",
+  },
+  {
+    id: 10,
+    title: "Project 10",
+    description: "Project 10",
+    pic: htmlP10,
+    tag: "html",
+    viewLink: "https://unique-griffin-6ae92b.netlify.app/",
+    sourceCode: "https://github.com/Aniket-ap/HTML_CSS__project-10",
+  },
+  {
+    id: 11,
+    title: "Project 11",
+    description: "Project 11",
+    pic: htmlP11,
+    tag: "html",
+    viewLink: "https://snazzy-jalebi-22cd89.netlify.app/",
+    sourceCode: "https://github.com/Aniket-ap/HTML_CSS__project-11",
+  },
+  {
+    id: 12,
+    title: "Project 12",
+    description: "Project 12",
+    pic: htmlP12,
+    tag: "html",
+    viewLink: "https://euphonious-banoffee-b1845c.netlify.app/",
+    sourceCode: "https://github.com/Aniket-ap/HTML_CSS__project-12",
+  },
+  {
+    id: 13,
+    title: "Project 13",
+    description: "Project 13",
+    pic: htmlP13,
+    tag: "html",
+    viewLink: "https://peaceful-buttercream-7b18f4.netlify.app/",
+    sourceCode: "https://github.com/Aniket-ap/HTML_CSS__project-13",
+  },
+  {
+    id: 14,
+    title: "Project 14",
+    description: "Project 14",
+    pic: htmlP14,
+    tag: "html",
+    viewLink: "https://lustrous-kangaroo-df0e07.netlify.app/",
+    sourceCode: "https://github.com/Aniket-ap/HTML_CSS__project-14",
+  },
+  {
+    id: 15,
+    title: "Project 15",
+    description: "Project 15",
+    pic: htmlP15,
+    tag: "html",
+    viewLink: "https://beautiful-druid-1879a1.netlify.app/",
+    sourceCode: "https://github.com/Aniket-ap/HTML_CSS__project-15",
+  },
 ];
+
+// {
+//   id: 1,
+//   title: "My Portfolio",
+//   description: "My Portfolio website using React Js.",
+//   pic: portfolioImg,
+//   viewLink: "https://aniket08-portfolio.netlify.app/",
+//   sourceCode: "https://github.com/Aniket-ap/my-portfolio",
+// },
+// {
+//   id: 2,
+//   title: "BlogHash",
+//   description: "A blog app where user user can post blog.it is a MERN stack project",
+//   pic: blogHashImg,
+//   viewLink: "https://helpful-medovik-b34862.netlify.app/",
+//   sourceCode: "https://github.com/Aniket-ap/my-portfolio",
+// },
+// {
+//   id: 3,
+//   title: "Github Search",
+//   description: "A github profile search app",
+//   pic: githubSearchImg,
+//   viewLink: "https://github-search-profile-app.netlify.app/",
+//   sourceCode: "https://gitlab.com/Aniket-ap/github-search-app",
+// },
+// {
+//   id: 4,
+//   title: "IMDB Clone",
+//   description: "A clone of popular movie review website IMDB using React Js.",
+//   pic: imdbImg,
+//   viewLink: "https://pedantic-ptolemy-a223c9.netlify.app/",
+//   sourceCode: "https://github.com/Aniket-ap/imdb-clone",
+// },
+// {
+//   id: 5,
+//   title: "SpaceX launch data",
+//   description: "An application having all the details of all spacex rocket launch data.",
+//   pic: spaceX,
+//   viewLink: "https://launch-spacex-data.netlify.app/",
+//   sourceCode: "https://github.com/Aniket-ap/techdome-assignment",
+// },
+// {
+//   id: 6,
+//   title: "TIC TAC TOE",
+//   description: "A simple tic tac toe game made using pure HTML, CSS3, JS.",
+//   pic: ticTacToeImg,
+//   viewLink: "https://aniket-ap.github.io/TicTacToe.github.io/",
+//   sourceCode: "https://github.com/Aniket-ap/TicTacToe.github.io",
+// }
