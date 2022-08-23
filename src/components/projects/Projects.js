e0502590a8a71272979b8b825827e955da21dccc
Aniket Pradhan <aniketpradhan814@gmail.com>
2022-08-23 20:11:35 +0530

    changes card style

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -6,10 +6,35 @@ import { Link } from "react-router-dom";
 const Projects = () => {
   return (
     <section className="all-projects">
-      <div className="projects">
+      <div className="grid1">
         {PROJECT_DETAILS.map((data) => (
           <React.Fragment key={data.id}>
-            <div className="project__card">
+            <article>
+              <img src={data.pic} alt="img" />
+              <div className="text">
+                {/* <h3>CARD 1</h3> */}
+                <Link
+                  className="pro-lnk"
+                  to="#"
+                  onClick={() =>
+                    (window.location = `${data.viewLink}`)
+                  }
+                >
+                  Live project
+                </Link>
+
+                <Link
+                  className="pro-lnk"
+                  to="#"
+                  onClick={() =>
+                    (window.location = `${data.sourceCode}`)
+                  }
+                >
+                  Source code
+                </Link>
+              </div>
+            </article>
+            {/* <div className="project__card">
               <div className="card__img">
                 <img src={data.pic} alt="" />
               </div>
@@ -31,7 +56,7 @@ const Projects = () => {
                   Source code
                 </Link>
               </div>
-            </div>
+            </div> */}
           </React.Fragment>
         ))}
       </div>
